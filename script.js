4b61ddb3caac9cc7182d961bf8bd965a6e2d5406
Luciano Simas Junior <luciano.simasjr@gmail.com>
2026-08-21 21:53:50 -0300

    Add: conteudos da aula de hoje- css

---
+++ script.js
@@ -0,0 +1,9 @@
+const formulario = document.getElementById("formulario-livro");
+
+
+formulario.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const titulo = document.getElementById('titulo').value;
+    
+})
